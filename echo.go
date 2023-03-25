d656356302e2120090c502d054998b019d3f6ba3
Konstantin <vorobey57ru@gmail.com>
2023-03-25 13:41:27 +0300

    v0.1

---
+++ echo.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	var n int
+	fmt.Print("Введите целое число: ")
+	_, err := fmt.Scan(&n)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Вы ввели число: %d\n", n)
+}
